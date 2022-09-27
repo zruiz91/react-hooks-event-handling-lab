0bcd768ba5ae135447c01b8a22c2e1c76c963bdd
Zach Ruiz <powersbooth10@gmail.com>
2022-09-26 21:17:10 -0500

    commit tree 62518eaf43e76e3742ce39fac808aed6e61f55a1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+
+
+function EyesOnMe() {
+
+    function eyesFocus(){
+        console.log('Good!')
+    }
+
+    function eyesBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+            <button onFocus={eyesFocus} onBlur={eyesBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,33 @@
 // Code Keypad Component Here
 
+
+// function Keypad() {
+
+//     return (
+//         <div>
+//             <form>
+//                 <input type="password" onChange={console.log("rntering password...")}/>
+//             </form>
+//         </div>
+//     )
+// }
+
+
 function Keypad (){
+
+    function enterPassword() {
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div>
+            <form>
+            <input onChange={enterPassword} type="password"></input>
+            </form>
+        </div>
     )
 }
 
 export default Keypad;
+
+
